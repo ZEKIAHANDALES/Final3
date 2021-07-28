ba669e71780a43def8fff03216ec69885e1573c1
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-28 22:55:17 +0800

    Update order.php

--- order.php
+++ order.php
@@ -167,4 +167,31 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     <!-- fOOD sEARCH Section Ends Here -->
 
     <!-- social Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/zaxxunorg/"><img src="https://img.icons8.com/cute-clipart/64/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/zaxxun/"><img src="https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/zaxxun"><img src="https://img.icons8.com/cute-clipart/64/000000/twitter.png"/></a>
+                </li>   
+            </ul>
+        </div>
+    </section>
+    <!-- Social Section Ends Here -->
+
+    <!-- Footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved.</p>
+        </div>
+    </section>
+    <!-- Footer Search Section Ends Here -->
+	
+</body>
+</html>
 ?>
