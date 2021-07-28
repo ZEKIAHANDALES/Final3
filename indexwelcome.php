83d01cff2c56b5908abb2953d25b61a296560fdc
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-28 21:28:45 +0800

    Update indexwelcome.php

--- indexwelcome.php
+++ indexwelcome.php
@@ -1,19 +1,19 @@
-<?php ?>
+<?php 
 
  //session needed to be changed if deploying online for this is from local database
     session_start();
     
-        //create cnstnts to store non repeating values
-      
-//Get Heroku ClearDB connection information
-define('SITEURL', 'http://localhost/zaxxun-ordering/');
-        define('LOCALHOST','localhost');
-        define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
-        define('DB_NAME', 'zaxxun_ordering');
-
-        $conn = mysqli_connect(LOCALHOST, DB_USERNAME,DB_PASSWORD) or die(mysqli_error());
-        $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+ //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 
 <!DOCTYPE html>
 <html lang = "en">
