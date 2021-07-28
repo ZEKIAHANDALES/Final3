f2e5d3d1964596d14cb87c267bbe2fd9887d4dd6
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-28 22:28:44 +0800

    Update categories.php

--- categories.php
+++ categories.php
@@ -1,5 +1,23 @@
-<?php include('partials-front/menu.php') ?>
+<?php 
+<?php 
 
+    //session needed to be changed if deploying online for this is from local database
+    session_start();
+    
+        //create cnstnts to store non repeating values
+      
+//Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 
 
     <!-- CAtegories Section Starts Here -->
@@ -68,4 +86,4 @@
 
 
     <!-- social Section Starts Here -->
-   <?php include('partials-front/footer.php') ?>
+  ?>
