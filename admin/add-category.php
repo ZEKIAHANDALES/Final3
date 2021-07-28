0909c26e4b6b2a263e7488ef00fd6f299ff84ece
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-28 18:46:08 +0400

    Update add-category.php

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,4 +1,58 @@
-<?php include('partials/menu.php');?>
+<?php 
+
+    //session needed to be changed if deploying online for this is from local database
+    session_start();
+    
+        //create cnstnts to store non repeating values
+      
+//Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>zaxuun admin</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+    <!--menu section starts-->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Log Out</a></li>
+            </ul>
+        </div>
+        
+    </div>
+    <!--menu section ends-->
+
+<?php 
+
+    //auth access control
+    if(!isset($_SESSION['user']))
+    {
+
+        $_SESSION['no-login-message'] = "<div class='error text-center'> Please Login to access </div>";
+        header('admin/login.php');
+
+    }
+
+?>
 
 <div class="main-content">
     <div class="wrapper">
@@ -133,7 +187,7 @@
                 if($upload==false)
                 {
                     $_SESSION['upload'] = "<div class='error'>Failed to upload image</div>";
-                    header('location:'.SITEURL.'admin/add-category.php');
+                    header('admin/add-category.php');
 
                     die();
 
@@ -162,14 +216,14 @@
 
                 $_SESSION['add'] = "<div class='success'>Category added successfully.</div>";
 
-                header('location:'.SITEURL.'admin/manage-category.php');
+                header('admin/manage-category.php');
 
             }
             else
             {
                 $_SESSION['add'] = "<div class='error'>Failed to add category </div>";
 
-                header('location:'.SITEURL.'admin/add-category.php');
+                header('admin/add-category.php');
 
 
             }  
@@ -182,4 +236,13 @@
     </div>
 </div>
 
-<?php include('partials/footer.php'); ?>
+<!--footer sec starts-->
+    <div class="footer">
+        <div class="menu">
+        <div class="wrapper">
+        <p class="text-center">2021 All rights reserved.</p>
+        </div>
+    </div>
+    <!--footer sec ends-->
+</body>
+</html>
