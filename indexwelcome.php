4b7396b0b89c4a120f8820457adbe62490f8d7b4
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-28 17:40:02 +0400

    Update indexwelcome.php

--- indexwelcome.php
+++ indexwelcome.php
@@ -1,21 +1,6 @@
 <!DOCTYPE html>
 <html lang = "en">
 <head>
-<?php
-//session needed to be changed if deploying online for this is from local database
-    session_start();
-    
- //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
     <meta charset="utf-8">
     <meta name = "viewport" content="width=device-width, initial-scale=1.0">
     <title>ZAXXUN ROBOT CAFE</title>
@@ -235,7 +220,6 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
         </div>
     </section>
     <!-- Footer Search Section Ends Here -->
-?>
 </body>
 </html>
 
