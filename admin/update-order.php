b04428b92d2d4cdeffc8603e18687082b023f9af
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-29 10:10:56 +0400

    Update update-order.php

--- admin/update-order.php
+++ admin/update-order.php
@@ -1,4 +1,58 @@
-<?php include('partials/menu.php');?>
+<?php 
+
+    //session needed to be changed if deploying online for this is from local database
+    session_start();
+    
+        //create cnstnts to store non repeating values
+      
+//Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
+
+<?php 
+
+    //auth access control
+    if(!isset($_SESSION['user']))
+    {
+
+        $_SESSION['no-login-message'] = "<div class='error text-center'> Please Login to access </div>";
+        header('location: login.php');
+
+    }
+
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>zaxuun admin</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+    <!--menu section starts-->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Log Out</a></li>
+            </ul>
+        </div>
+        
+    </div>
+    <!--menu section ends-->
 
 <div class="main-content">
     <div class="wrapper">
@@ -27,12 +81,12 @@
                 }
                 else
                 {
-                    header('location:'.SITEURL.'admin/manage-order.php');
+                    header('location: manage-order.php');
                 }
             }
             else
             {
-                header('location:'.SITEURL.'admin/manage-order.php');
+                header('location: manage-order.php');
             }
 
         ?>
@@ -110,12 +164,12 @@
                  if($res2==true)
                  {
                     $_SESSION['update'] = "<div class='success'>Order Updated Successfully </div>";
-                header('location: '.SITEURL.'admin/manage-order.php');
+                header('location: manage-order.php');
                  }
                  else
                  {
                    $_SESSION['update'] = "<div class='error'>Failed to updated order </div>";
-                header('location: '.SITEURL.'admin/manage-order.php'); 
+                header('location: manage-order.php'); 
                  }
             }
 
@@ -126,4 +180,13 @@
     </div>
 </div>
 
-<?php include('partials/footer.php');?>
+<!--footer sec starts-->
+    <div class="footer">
+        <div class="menu">
+        <div class="wrapper">
+        <p class="text-center">2021 All rights reserved.</p>
+        </div>
+    </div>
+    <!--footer sec ends-->
+</body>
+</html>
