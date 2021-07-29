77ece95630bb6d1edf8c4f650f4b8e48e437c3b7
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-29 09:12:14 +0400

    Update manage-admin.php

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,4 +1,57 @@
-<?php include('partials/menu.php');?>
+<?php 
+
+    //session needed to be changed if deploying online for this is from local database
+    session_start();
+    
+        //create cnstnts to store non repeating values
+      
+//Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
+<?php 
+
+    //auth access control
+    if(!isset($_SESSION['user']))
+    {
+
+        $_SESSION['no-login-message'] = "<div class='error text-center'> Please Login to access </div>";
+        header('location: login.php');
+
+    }
+
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>zaxuun admin</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+    <!--menu section starts-->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Log Out</a></li>
+            </ul>
+        </div>
+        
+    </div>
+
 
     <!--menu section ends-->
 
@@ -96,9 +149,9 @@
                                 <td><?php echo $full_name; ?></td>
                                 <td><?php echo $username; ?></td>
                                 <td>
-                                    <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
-                                    <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>"class="btn-secondary">Update Admin</a>
-                                    <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>"class="btn-danger">Delete Admin</a>
+                                    <a href="admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
+                                    <a href="admin/update-admin.php?id=<?php echo $id; ?>"class="btn-secondary">Update Admin</a>
+                                    <a href="admin/delete-admin.php?id=<?php echo $id; ?>"class="btn-danger">Delete Admin</a>
                                 </td>
                             </tr>
 
@@ -121,5 +174,13 @@
     </div>
  
     <!--main content sec ends-->
-
-<?php include('partials/footer.php');?>
+<!--footer sec starts-->
+    <div class="footer">
+        <div class="menu">
+        <div class="wrapper">
+        <p class="text-center">2021 All rights reserved.</p>
+        </div>
+    </div>
+    <!--footer sec ends-->
+</body>
+</html>
