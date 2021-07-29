16c8fcb9c8e1b8038e3d268e82ac4b6172e7ceae
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-29 09:03:36 +0400

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -25,10 +25,7 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     {
 
         $_SESSION['no-login-message'] = "<div class='error text-center'> Please Login to access </div>";
-        echo "<script>window.location.href='login.php'</script>";
-        die();
         
-
     }
 
 ?>
@@ -99,14 +96,12 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
             $_SESSION['user'] = $username;
 
             echo "<script>window.location.href='index.php'</script>";
-            die();
 
         }
         else
         {
             $_SESSION['login'] = "<div class='error'>User could not be found.</div>";
             echo "<script>window.location.href='login.php'</script>";
-            die();
         }
 
     }
