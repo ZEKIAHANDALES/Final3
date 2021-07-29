e63f1ef999004e0a231ec21ed8486d992c6f785e
ZEKIAHANDALES <87362100+ZEKIAHANDALES@users.noreply.github.com>
2021-07-29 10:02:07 +0400

    Update delete-food.php

--- admin/delete-food.php
+++ admin/delete-food.php
@@ -1,5 +1,30 @@
 <?php 
-    include('../config/constants.php');
+
+    //session needed to be changed if deploying online for this is from local database
+    session_start();
+    
+        //create cnstnts to store non repeating values
+      
+//Get Heroku ClearDB connection information
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+//auth access control
+    if(!isset($_SESSION['user']))
+    {
+
+        $_SESSION['no-login-message'] = "<div class='error text-center'> Please Login to access </div>";
+        header('location: login.php');
+
+    }
 
         if (isset($_GET['id']) AND isset($_GET['image_name'])) 
     {
@@ -16,7 +41,7 @@
             {
                 $_SESSION['remove'] = "<div class='error'>Failed to remove Food Image </div>";  
 
-                header('location:'.SITEURL.'admin/manage-food.php'); 
+                header('location: manage-food.php'); 
                 die();
 
             }
@@ -32,14 +57,14 @@
         {
 
             $_SESSION['delete'] = "<div class='success'>Food Deleted Successfully </div>";
-            header('location:'.SITEURL.'admin/manage-food.php');
+            header('location: manage-food.php');
             
         }
         else
         {
 
             $_SESSION['delete'] = "<div class='error'>Failed to delete food </div>";
-            header('location:'.SITEURL.'admin/manage-food.php');
+            header('location: manage-food.php');
 
         }
 
@@ -48,7 +73,7 @@
     else
     {
         $_SESSION['anauth'] = "<div class='error'>Unauthorized Access </div>";
-        header('location:'.SITEURL.'admin/manage-food.php');
+        header('location: manage-food.php');
 
     }
 ?>
